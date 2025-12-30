2b8a09a4cd409295e52e0531ee8fede269a03cfe
awsless <854771076@qq.com>
2025-12-30 18:17:40 +0800

    refactor: replace manual Ant Design CSS-in-JS setup with `@ant-design/nextjs-registry` and update ESLint dependencies

--- src/lib/AntdRegistry.tsx
+++ src/lib/AntdRegistry.tsx
@@ -1,25 +1,19 @@
 'use client';
 
 import React from 'react';
-import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
-import type Entity from '@ant-design/cssinjs/es/Cache';
-import { useServerInsertedHTML } from 'next/navigation';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 import { ConfigProvider } from 'antd';
 import enUS from 'antd/locale/en_US';
 import zhCN from 'antd/locale/zh_CN';
 import { Locale } from '@/i18n/config';
 
 const StyledComponentsRegistry = ({ children, locale }: { children: React.ReactNode; locale: Locale }) => {
-    const cache = React.useMemo<Entity>(() => createCache(), []);
-    useServerInsertedHTML(() => (
-        <style id="antd" dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }} />
-    ));
     return (
-        <StyleProvider cache={cache}>
+        <AntdRegistry>
             <ConfigProvider locale={locale === 'zh' ? zhCN : enUS}>
                 {children}
             </ConfigProvider>
-        </StyleProvider>
+        </AntdRegistry>
     );
 };
 
