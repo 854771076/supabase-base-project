db90194f273b7be5885a3c306e0bfe6d1b4231ac
awsless <854771076@qq.com>
2025-12-31 16:06:23 +0800

    2

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next'
 import { getURL } from '@/utils/url'
-
+export const dynamic = 'force-dynamic';
 export default function robots(): MetadataRoute.Robots {
     return {
         rules: {
